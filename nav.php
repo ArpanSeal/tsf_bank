cb7d6850c6ca9a00bd71a146ba78b33350c79d99
Arpan <arpanseal1234@gmail.com>
2022-09-09 11:27:19 +0530

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TSF Bank</title>
-  <link rel="stylesheet" type="text/css" href="css/index.css">
-  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
-  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #50C878;">
-    <div class="container-fluid">
-      <a class="navbar-brand logo" href="index.php"><img src="img/logo.png" alt="logo" height=50px width=50px></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TSF Bank</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+  </head>
+
+  
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #50C878;">
+      <div class="container-fluid">
+        <a class="navbar-brand logo" href="index.php"><img src="img/logo.png" alt="logo" height=50px width=50px></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 
 
-        <ul class="navbar-nav">
-          <li class="nav-item btn btn-secondary">
-            <!-- <a class="nav-link text-white" href="index.php">Home</a> -->
-            <a class="nav-link text-white" href="index.php" style="
-            margin: auto;">Home</a>
-          </li>
-          <li class="nav-item btn btn-secondary">
-            <a class="nav-link text-white" href="https://www.linkedin.com/in/arpan-seal-9a27b6210/" target="_blank" style="margin: auto;">About Me</a>
-          </li>
-        </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item btn btn-secondary">
+              <!-- <a class="nav-link text-white" href="index.php">Home</a> -->
+              <a class="nav-link text-white" href="index.php" style="
+              margin: auto;">Home</a>
+            </li>
+            <li class="nav-item btn btn-secondary">
+              <a class="nav-link text-white" href="https://www.linkedin.com/in/arpan-seal-9a27b6210/" target="_blank" style="margin: auto;">About Me</a>
+            </li>
+          </ul>
+
+        </div>
 
       </div>
-
-    </div>
-  </nav>
-</body>
+    </nav>
+  </body>
 </html>
