ea763867db04259cffbbb7e1d53dd906f7b1bd80
Arpan <arpanseal1234@gmail.com>
2023-02-23 21:24:15 +0530

    tsfBank

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-//Connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tsf_bank";
+	//Connection
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "tsf_bank";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$conn) {
-	die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
-}
+	if (!$conn) {
+		die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
+	}
+?>
