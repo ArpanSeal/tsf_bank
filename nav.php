e1bdee272a0d6e2020eafcc093df44512c204ef7
Arpan <arpanseal1234@gmail.com>
2023-02-27 15:04:17 +0530

    tsfBank

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -34,4 +34,23 @@
   </nav>
 </body>
 
-</html>
+</html> -->
+
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand logo" href="index.php"><img src="img/logo.png" alt="logo" height=50px width=50px></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="togglerIcon navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item nbtn btnc my-1">
+          <a class="nav-link" href="index.php" style="margin: auto; color: #212529;">Home</a>
+        </li>
+        <li class="nav-item nbtn btnc my-1">
+          <a class="nav-link" href="https://www.linkedin.com/in/arpan-seal-9a27b6210/" target="_blank" style="margin: auto; color: #212529;">About Me</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
